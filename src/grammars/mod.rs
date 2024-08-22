f5e5192a6d80c1b200387ec7ce55b611d93a38e1
fire833 <kttpsy@gmail.com>
2024-08-22 17:11:57 -0500

    feat: get a better idea of what I am doing

--- src/grammars/mod.rs
+++ src/grammars/mod.rs
@@ -16,42 +16,67 @@
 *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-use std::fmt::{Debug, Display};
+use std::{collections::BTreeMap, fmt::Debug};
 
-pub type Grammar<T, I> = Productions<T, I>;
+pub trait BinarySerialize {
+    fn serialize(&self) -> [u8];
+}
+
+pub struct Grammar<T, I>
+where
+    T: Sized + Clone + Debug + BinarySerialize, // Generic terminal type, this will usually be some kind of string or bytes.
+    I: Sized + Clone + Debug,                   // Generic ident non-terminal type.
+{
+    /// The root symbol of this grammar definition.
+    root: I,
+
+    /// The list of productions associated with this grammar.
+    productions: BTreeMap<I, Production<T, I>>,
+}
 
 impl<T, I> Grammar<T, I>
 where
-    T: Sized + Clone, // Generic terminal type, this will usually be some kind of string or bytes.
-    I: Sized + Clone, // Generic ident non-terminal type.
+    T: Sized + Clone + Debug + BinarySerialize, // Generic terminal type, this will usually be some kind of string or bytes.
+    I: Sized + Clone + Debug,                   // Generic ident non-terminal type.
 {
 }
 
-pub struct Productions<T, I>
+pub type ProductionRule<T, I> = Vec<GrammarElement<T, I>>;
+
+/// Represents all the expansion rules for a particular non-terminal
+/// identifier.
+pub struct Production<T, I>
 where
-    T: Sized + Clone, // Generic terminal type, this will usually be some kind of string or bytes.
-    I: Sized + Clone, // Generic ident non-terminal type.
+    T: Sized + Clone + Debug + BinarySerialize, // Generic terminal type, this will usually be some kind of string or bytes.
+    I: Sized + Clone + Debug,                   // Generic ident non-terminal type.
 {
-    items: Vec<GrammarElement<T, I>>,
+    /// Reference to the non-terminal that we are using here.
+    non_terminal: I,
+
+    /// The list of all production rules (ie vectors of vectors of symbols
+    /// that can be expanded upon in the grammar expansion process).
+    items: Vec<ProductionRule<T, I>>,
 }
 
-impl<T, I> Productions<T, I>
+impl<T, I> Production<T, I>
 where
-    T: Sized + Clone + Debug, // Generic terminal type, this will usually be some kind of string or bytes.
-    I: Sized + Clone + Display, // Generic ident non-terminal type.
+    T: Sized + Clone + Debug + BinarySerialize, // Generic terminal type, this will usually be some kind of string or bytes.
+    I: Sized + Clone + Debug,                   // Generic ident non-terminal type.
 {
-    pub fn new(items: Vec<GrammarElement<T, I>>) -> Self {
-        Self { items }
+    pub fn new(non_terminal: I, items: Vec<ProductionRule<T, I>>) -> Self {
+        Self {
+            items,
+            non_terminal,
+        }
     }
 }
 
 pub enum GrammarElement<T, I>
 where
-    T: Sized + Clone, // Generic terminal type, this will usually be some kind of string or bytes.
-    I: Sized + Clone, // Generic ident non-terminal type.
+    T: Sized + Clone + Debug + BinarySerialize, // Generic terminal type, this will usually be some kind of string or bytes.
+    I: Sized + Clone + Debug,                   // Generic ident non-terminal type.
 {
     Terminal(T),
-    NonTerminal(I, Productions<T, I>),
-    NonTerminalRef(I),
+    NonTerminal(I),
     Epsilon,
 }
