0f6b9a3d164323f37655b7a4740b3b8d4e770566
fire833 <kttpsy@gmail.com>
2024-09-15 20:09:22 -0500

    feat(taco): complete more of the grammar

--- src/languages/taco_schedule.rs
+++ src/languages/taco_schedule.rs
@@ -25,7 +25,9 @@ use super::strings::{nterminal_str, terminal_str, StringValue, COMMA, EPSILON, L
 // Non terminals for this grammar.
 nterminal_str!(NT_ENTRYPOINT, "entrypoint");
 nterminal_str!(NT_RULE, "rule");
-nterminal_str!(NT_ASSEMBLE_RULE, "assemble_rule");
+nterminal_str!(NT_ASSEMBLE_STRATEGY, "assemble_strategy");
+nterminal_str!(NT_PARALLELIZE_HW, "parallelize_hw");
+nterminal_str!(NT_PARALLELIZE_RACES, "parallelize_races");
 nterminal_str!(NT_INDEX_VARIABLE, "index_variable");
 
 // Terminals for this grammar.
@@ -40,9 +42,22 @@ terminal_str!(BOUND_OP, "bound");
 terminal_str!(PARALLELIZE_OP, "parallelize");
 terminal_str!(ASSEMBLE_OP, "assemble");
 
-// Terminal arguments for this grammar
+// Terminal Assemble operation arguments for this grammar
 terminal_str!(ASSEMBLE_APPEND, "Append");
 terminal_str!(ASSEMBLE_INSERT, "Insert");
+
+// Terminal Parallelize Hardware operation arguments for this grammar
+terminal_str!(PARALLELIZE_HW_NOTPARALLEL, "NotParallel");
+terminal_str!(PARALLELIZE_HW_CPUTHREAD, "CPUThread");
+terminal_str!(PARALLELIZE_HW_CPUVECTOR, "CPUVector");
+terminal_str!(PARALLELIZE_HW_GPUWARP, "GPUWarp");
+
+// Terminal Parallelize Race Strategy operation arguments for this grammar.
+terminal_str!(PARALLELIZE_RACE_IGNORE, "IgnoreRaces");
+terminal_str!(PARALLELIZE_RACE_NORACES, "NoRaces");
+terminal_str!(PARALLELIZE_RACE_ATOMICS, "Atomics");
+terminal_str!(PARALLELIZE_RACE_TEMP, "Temporary");
+terminal_str!(PARALLELIZE_RACE_PREDUCE, "ParallelReduction");
 
 pub struct TacoGrammar {
     index_variables: Vec<String>,
@@ -93,30 +108,60 @@ impl TacoGrammar {
                             COMMA,
                             RPAREN
                         ),
-                        // split()
-                        production_rule!(SPLIT_OP, LPAREN, NT_INDEX_VARIABLE, COMMA, RPAREN), // Incomplete
+                        // split(index_variable, outer_index_variable, inner_index_variable, split_factor)
+                        production_rule!(SPLIT_OP, LPAREN, NT_INDEX_VARIABLE, COMMA, RPAREN),
                         // reorder(index_variable)
                         production_rule!(REORDER_OP, LPAREN, NT_INDEX_VARIABLE, RPAREN),
-                        // divide()
-                        production_rule!(DIVIDE_OP, LPAREN, RPAREN),
-                        // precompute()
+                        // divide(index_variable, outer_index_variable, inner_index_variable, divide_factor)
+                        production_rule!(DIVIDE_OP, LPAREN, NT_INDEX_VARIABLE, COMMA, RPAREN),
+                        // precompute(expr, index_variable, workspace_index_variable)
                         production_rule!(PRECOMPUTE_OP, LPAREN, RPAREN),
-                        // unroll()
-                        production_rule!(UNROLL_OP, LPAREN, RPAREN),
-                        // bound()
+                        // unroll(index_variable, unroll_factor)
+                        production_rule!(UNROLL_OP, LPAREN, NT_INDEX_VARIABLE, COMMA, RPAREN),
+                        // bound() // appears to be not yet supported in taco.
                         production_rule!(BOUND_OP, LPAREN, RPAREN),
-                        // parallelize()
-                        production_rule!(PARALLELIZE_OP, LPAREN, RPAREN),
+                        // parallelize(index_variable, hardware, race_strategy)
+                        production_rule!(
+                            PARALLELIZE_OP,
+                            LPAREN,
+                            NT_INDEX_VARIABLE,
+                            COMMA,
+                            NT_PARALLELIZE_HW,
+                            COMMA,
+                            NT_PARALLELIZE_RACES,
+                            RPAREN
+                        ),
                         // assemble()
-                        production_rule!(ASSEMBLE_OP, LPAREN, COMMA, NT_ASSEMBLE_RULE, RPAREN),
+                        production_rule!(ASSEMBLE_OP, LPAREN, COMMA, NT_ASSEMBLE_STRATEGY, RPAREN),
                     ],
                 ),
                 // Assemble Operation Rule.
                 Production::new(
-                    ProductionLHS::new_context_free_elem(NT_ASSEMBLE_RULE),
+                    ProductionLHS::new_context_free_elem(NT_ASSEMBLE_STRATEGY),
                     vec![
                         production_rule!(ASSEMBLE_APPEND),
                         production_rule!(ASSEMBLE_INSERT),
+                    ],
+                ),
+                // Parallelize Hardware Rule.
+                Production::new(
+                    ProductionLHS::new_context_free_elem(NT_PARALLELIZE_HW),
+                    vec![
+                        production_rule!(PARALLELIZE_HW_CPUTHREAD),
+                        production_rule!(PARALLELIZE_HW_CPUVECTOR),
+                        production_rule!(PARALLELIZE_HW_GPUWARP),
+                        production_rule!(PARALLELIZE_HW_NOTPARALLEL),
+                    ],
+                ),
+                // Parallelize Race Conditions Rule
+                Production::new(
+                    ProductionLHS::new_context_free_elem(NT_PARALLELIZE_RACES),
+                    vec![
+                        production_rule!(PARALLELIZE_RACE_IGNORE),
+                        production_rule!(PARALLELIZE_RACE_TEMP),
+                        production_rule!(PARALLELIZE_RACE_NORACES),
+                        production_rule!(PARALLELIZE_RACE_PREDUCE),
+                        production_rule!(PARALLELIZE_RACE_ATOMICS),
                     ],
                 ),
                 // Index Variable Rule,
