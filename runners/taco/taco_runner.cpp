88166bbdb5f6f6ed1978f0a24cb9d754f433b142
fire833 <kttpsy@gmail.com>
2024-09-22 17:53:20 -0500

    feat(taco): work on getting the runner working
    We can get random tensors, but they are not populated, and cmake is
    being a lovely pain in the rear with linking on libtaco.

--- runners/taco/taco_runner.cpp
+++ runners/taco/taco_runner.cpp
@@ -1,28 +1,91 @@
 
+#include "random"
 #include "taco/format.h"
 #include "taco/index_notation/index_notation.h"
 #include "taco/tensor.h"
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
 using namespace taco;
 using namespace std;
 
+vector<int> parseDims(const string &s) {
+  vector<int> values;
+  stringstream stream(s);
+  string tok;
+
+  while (getline(stream, tok, ',')) {
+    values.push_back(stoi(tok));
+  }
+
+  return values;
+}
+
+vector<ModeFormatPack> parseFormats(const string &s) {
+  vector<ModeFormatPack> values;
+  stringstream stream(s);
+  string tok;
+
+  while (getline(stream, tok, ',')) {
+    // Apparently we can't switch on strings in c++, marvelous right?
+    if (tok == "s") {
+      values.push_back(Sparse);
+    } else if (tok == "d") {
+      values.push_back(Dense);
+    } else if (tok == "c") {
+      values.push_back(Compressed);
+    } else if (tok == "g") {
+      values.push_back(Singleton);
+    }
+  }
+
+  return values;
+}
+
 int main(int argc, char *argv[]) {
   if (argc <= 1) {
-    std::cout << "this program requires arguments.";
+    cout << "this program requires arguments.\n";
     return 1;
   }
 
   // Dumb stuff to cast arguments to strings for easier parsing.
   string op = argv[1];
+  // Apparently we can't switch on strings in c++, marvelous right?
   if (op == "gentensor") {
+    if (argc != 5) {
+      cout << "gentensor <name> <dims> <formats>\n";
+      return 1;
+    }
+    string name = argv[2];
+    vector<int> dims = parseDims(argv[3]);
+    vector<ModeFormatPack> formats = parseFormats(argv[4]);
 
+    // Seed RNG with the current time approximately.
+    // srand(time(0));
+
+    Tensor<double> T(name, dims, Format(formats));
+    // Populate the tensor with some data.
+    for (int dim = dims.size() - 1; dim > 0; dim--) {
+      if (formats[dim] == Dense) {
+      } else if (formats[dim] == Sparse || formats[dim] == Compressed) {
+      } else if (formats[dim] == Singleton) {
+      }
+    }
+
+    cout << "packing generated tensor into correct format\n";
+    // Compress the tensor into the correct format.
+    T.pack();
+    cout << "packed generated tensor\n";
+
+    // save and write the tensor out.
+    stringstream ss;
+    ss << name << ".tns";
+    write(ss.str(), T);
+    return 0;
   } else if (op == "evaluate") {
   }
-
-  Tensor<long> A("A", {2048, 2048}, Format({Dense, Dense}));
-  write("A.tns", A);
 }
