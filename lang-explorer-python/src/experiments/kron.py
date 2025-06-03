c0bd414bcb1ea39f657adfd9d57fccbbbeb778c0
fire833 <kttpsy@gmail.com>
2025-06-03 10:13:13 -0500

    feat: kron works now

--- lang-explorer-python/src/experiments/kron.py
+++ lang-explorer-python/src/experiments/kron.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def find_kron_val(k, i, j, matrix):
 	if k == 0:
-		return matrix[i][j]
+		return matrix[int(i), int(j)]
 
 	rc = 2**(k+1)
 	rc2 = rc / 2
@@ -12,11 +12,11 @@ def find_kron_val(k, i, j, matrix):
 	if i >= rc2 and j >= rc2:
 		return matrix[1, 1] * find_kron_val(k - 1, i - rc2, j - rc2, matrix)
 	elif i < rc2 and j < rc2:
-		return matrix[0, 0] * find_kron_val(k - 1, i - 1, j - 1, matrix)
+		return matrix[0, 0] * find_kron_val(k - 1, i, j, matrix)
 	elif i < rc2 and j >= rc2:
-		return matrix[0, 1] * find_kron_val(k - 1, i - 1, j - rc2, matrix)
+		return matrix[0, 1] * find_kron_val(k - 1, i, j - rc2, matrix)
 	elif i >= rc2 and j < rc2:
-		return matrix[1, 0] * find_kron_val(k - 1, i - rc2, j - 1, matrix)
+		return matrix[1, 0] * find_kron_val(k - 1, i - rc2, j, matrix)
 
 def main():
 	kron = np.array([[0.1, 0.2], [0.3, 0.4]])
@@ -40,7 +40,7 @@ def main():
 	# for i in range(8):
 	# 	for j in range(8):
 	# 		if new[i, j] != canonical[i, j]:
-	# 			print("error!")
+	# 			print(f"error! {i} {j} {new[i, j]} {canonical[i, j]}")
 	
 if __name__ == "__main__":
 	main()
