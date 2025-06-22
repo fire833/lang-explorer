a11397a33617edd06abb1e3786a856ac55c1ed9c
fire833 <kttpsy@gmail.com>
2025-06-21 19:41:29 -0500

    feat: start on exhaustive search expander

---
+++ src/expanders/exhaustive.rs
@@ -0,0 +1,52 @@
+/*
+*	Copyright (C) 2025 Kendall Tauser
+*
+*	This program is free software; you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation; either version 2 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License along
+*	with this program; if not, write to the Free Software Foundation, Inc.,
+*	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+use crate::{
+    errors::LangExplorerError,
+    expanders::GrammarExpander,
+    grammar::{prod::Production, rule::ProductionRule, Grammar, NonTerminal, Terminal},
+};
+
+pub struct ExhaustiveExpander {}
+
+impl<T, I> GrammarExpander<T, I> for ExhaustiveExpander
+where
+    T: Terminal,
+    I: NonTerminal,
+{
+    /// We may need to initialize the expander depending on the type of grammar
+    /// we are using. For example, with my ML based example, the internal models of
+    /// the expander may change completely depending on the rules of the grammar
+    /// I want to expand.
+    fn init<'a>(grammar: &'a Grammar<T, I>) -> Result<Self, LangExplorerError>
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
+
+    /// We want the expander to take a grammar and the current rule and
+    /// make a decision on what the next expansion should be.
+    fn expand_rule<'a>(
+        &mut self,
+        grammar: &'a Grammar<T, I>,
+        production: &'a Production<T, I>,
+    ) -> &'a ProductionRule<T, I> {
+        todo!()
+    }
+}
