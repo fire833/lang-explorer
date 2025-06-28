4756aefe76ef0d34736bc6f70f84de50df5a1be3
fire833 <kttpsy@gmail.com>
2025-06-28 13:17:26 -0500

    feat: well, got some embeddings

--- lang-explorer-python/main.py
+++ lang-explorer-python/main.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 
-from src.utils.client import generate, GenerateParams, GenerateResults, CSSLanguageParameters, TacoScheduleParameters, TacoExpressionParameters
+from src.utils.client import GenerateParams, GenerateResults, CSSLanguageParameters, TacoScheduleParameters, TacoExpressionParameters
 from argparse import ArgumentParser	
 import sys
+from src.commands.embeddings import generate_embeddings
 
 def main():
 	parser = ArgumentParser(description="Lang-Explorer Python stuff")
+	sub = parser.add_subparsers(dest="cmd", required=True)
+
+	embed = sub.add_parser("embedding_gen", help="Generate embeddings.")
+	embed.add_argument("--output-path", nargs="?", default="results/embeddings.csv", help="Embeddings path.")
+	embed.add_argument("--dimensions", type=int, default=128, help="Number of dimensions. Default is 128.")
+	embed.add_argument("--epochs", type=int, default=10, help="Number of epochs. Default is 10.")
+	embed.add_argument("--min-count", type=int, default=5, help="Minimal structural feature count. Default is 5.")
+	embed.add_argument("--wl-iterations", type=int, default=2, help="Number of Weisfeiler-Lehman iterations. Default is 2.")
+	embed.add_argument("--learning-rate", type=float, default=0.025, help="Initial learning rate. Default is 0.025.")
+	embed.add_argument("--down-sampling", type=float, default=0.0001, help="Down sampling rate of features. Default is 0.0001.")
+	embed.add_argument("--workers", type=int, default=8, help="Number of workers. Default is 8.")
+	embed.set_defaults(func=generate_embeddings)
 
 	args = parser.parse_args(sys.argv[1:])
-	# args.func(args)
-
-	res = generate("http://localhost:8080", "tacosched", "mc", 
-		GenerateParams(100000, True, True, False, 3, 
-		css=CSSLanguageParameters(["foo", "bar", "baz"], ["1", "2", "3"]),
-		# taco_expression=TacoExpressionParameters(),
-		# taco_schedule=TacoScheduleParameters(),
-		taco_schedule=TacoScheduleParameters(index_variables=["i"], workspace_index_variables=["j"], fused_index_variables=["k"], split_factor_variables=["l"], divide_factor_variables=["m"], unroll_factor_variables=["n"]),
-		taco_expression=TacoExpressionParameters([], [])))
-
-	print(res)
+	args.func(args)
 
 if __name__ == "__main__":
 	main()
--- lang-explorer-python/src/commands/embeddings.py
+++ lang-explorer-python/src/commands/embeddings.py
@@ -1,3 +1,53 @@
 
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from src.utils.client import generate, TacoExpressionParameters, TacoScheduleParameters, CSSLanguageParameters, GenerateParams, GenerateResults
+import os
+import pandas as pd
+
 def generate_embeddings(args):
-	pass
+	print("making call to explorer")
+	res = generate("http://localhost:8080", "tacosched", "mc", 
+		GenerateParams(100000, True, True, False, 3, 
+		css=CSSLanguageParameters(["foo", "bar", "baz"], ["1", "2", "3"]),
+		# taco_expression=TacoExpressionParameters(),
+		# taco_schedule=TacoScheduleParameters(),
+		taco_schedule=TacoScheduleParameters(index_variables=["i"], workspace_index_variables=["j"], fused_index_variables=["k"], split_factor_variables=["l"], divide_factor_variables=["m"], unroll_factor_variables=["n"]),
+		taco_expression=TacoExpressionParameters([], [])))
+
+	document_collections = []
+
+	print("extracting explorer response")
+	for i, feat in enumerate(res.features):
+		graph = res.programs[i]
+		doc = TaggedDocument(words=feat, tags=[graph])
+		document_collections.append(doc)
+
+	print("\nOptimization started.\n")
+
+	model = Doc2Vec(document_collections,
+		vector_size=args.dimensions,
+		window=0,
+		min_count=args.min_count,
+		dm=0,
+		sample=args.down_sampling,
+		workers=args.workers,
+		epochs=args.epochs,
+		alpha=args.learning_rate)
+
+	save_embedding(args.output_path, model, res.programs, args.dimensions)
+
+def save_embedding(output_path, model, programs, dimensions):
+    """
+    Function to save the embedding.
+    :param output_path: Path to the embedding csv.
+    :param model: The embedding model object.
+    :param files: The list of files.
+    :param dimensions: The embedding dimension parameter.
+    """
+    out = []
+    for prog in programs:
+        out.append([prog] + list(model.docvecs[prog]))
+    column_names = ["type"]+["x_"+str(dim) for dim in range(dimensions)]
+    out = pd.DataFrame(out, columns=column_names)
+    out = out.sort_values(["type"])
+    out.to_csv(output_path, index=None)
