42840422726f7b05ef4c38913fbf31b1550e7a99
fire833 <kttpsy@gmail.com>
2025-06-28 21:43:25 -0500

    feat: LOOK AT THAT

--- lang-explorer-python/src/commands/viz.py
+++ lang-explorer-python/src/commands/viz.py
@@ -10,14 +10,14 @@ def data_viz(args):
 		plain = file.removesuffix(".csv")
 		data = pd.read_csv(f"results/{plain}.csv", skiprows=1, delimiter=',')
 
-		embedding = TSNE(2).fit_transform(data.iloc[:,1:-1])
+		embedding = TSNE(2).fit_transform(data.iloc[:,1:])
 		plt.title(f"t-SNE (2D) of {plain} dataset")
 		plt.scatter(embedding[:,0], embedding[:,1], c=data.iloc[:,-1])
 		plt.savefig(f"{args.output}/tsne2d{plain}.png", dpi=500)
 		plt.tight_layout()
 		plt.close()
 
-		embedding = TSNE(3).fit_transform(data[:,1:-1])
+		embedding = TSNE(3).fit_transform(data.iloc[:,1:])
 		fig = plt.figure(figsize=(8,6))
 		ax = fig.add_subplot(111, projection='3d')
 		ax.scatter3D(embedding[:,0], embedding[:,1], embedding[:,2], c=data.iloc[:,-1])
