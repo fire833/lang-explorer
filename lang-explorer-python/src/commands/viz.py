374604cc63743a8c5ae7c2a6afeb28aade9b9c90
fire833 <kttpsy@gmail.com>
2025-06-28 22:14:41 -0500

    feat: rerun tsne where color corresponds to program length

--- lang-explorer-python/src/commands/viz.py
+++ lang-explorer-python/src/commands/viz.py
@@ -5,23 +5,31 @@ from sklearn.manifold import TSNE
 import os
 import pandas as pd
 
+def strlen(x):
+	return len(str(x))
+
 def data_viz(args):
 	for file in os.listdir("results/"):
 		plain = file.removesuffix(".csv")
-		data = pd.read_csv(f"results/{plain}.csv", skiprows=1, delimiter=',')
+		data = pd.read_csv(f"results/{plain}.csv", skiprows=0, delimiter=',')
 
-		embedding = TSNE(2).fit_transform(data.iloc[:,1:])
+		psize = [len(str(x)) for x in data["type"]]
+		data["plen"] = data["type"].apply(strlen)
+
+		embedding = TSNE(2).fit_transform(data.iloc[:,1:-1])
 		plt.title(f"t-SNE (2D) of {plain} dataset")
-		plt.scatter(embedding[:,0], embedding[:,1], c=data.iloc[:,-1])
-		plt.savefig(f"{args.output}/tsne2d{plain}.png", dpi=500)
+		plt.scatter(embedding[:,0], embedding[:,1], c=data["plen"])
+		plt.savefig(f"{args.output}/tsne2d{plain}colors.png", dpi=500)
 		plt.tight_layout()
+		plt.legend()
 		plt.close()
 
-		embedding = TSNE(3).fit_transform(data.iloc[:,1:])
+		embedding = TSNE(3).fit_transform(data.iloc[:,1:-1])
 		fig = plt.figure(figsize=(8,6))
 		ax = fig.add_subplot(111, projection='3d')
-		ax.scatter3D(embedding[:,0], embedding[:,1], embedding[:,2], c=data.iloc[:,-1])
+		ax.scatter3D(embedding[:,0], embedding[:,1], embedding[:,2], c=data["plen"])
 		ax.set_title(f"t-SNE (3D) of {plain} dataset")
-		plt.savefig(f"{args.output}/tsne3d{plain}.png", dpi=500)
+		plt.savefig(f"{args.output}/tsne3d{plain}colors.png", dpi=500)
 		plt.tight_layout()
+		plt.legend()
 		plt.close()
