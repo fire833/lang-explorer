be1c265634e70963f7b4440f892347762b266fed
fire833 <kttpsy@gmail.com>
2025-07-05 14:15:01 -0500

    fix: negative sampling works sort of, may be fundamentally wrong, but what's there works

--- src/tooling/modules/loss/nsampling.rs
+++ src/tooling/modules/loss/nsampling.rs
@@ -22,8 +22,11 @@ use burn::{
     config::Config,
     module::Module,
     prelude::Backend,
-    tensor::{activation::sigmoid, Float, Tensor},
+    tensor::{activation::sigmoid, s, Float, Tensor},
 };
+
+#[allow(unused)]
+use burn::backend::NdArray;
 
 #[derive(Debug, Config)]
 pub struct NegativeSamplingConfig {}
@@ -41,14 +44,28 @@ pub struct NegativeSampling<B: Backend> {
 
 impl<B: Backend> NegativeSampling<B> {
     pub fn forward(&self, input: Tensor<B, 2, Float>) -> Tensor<B, 1, Float> {
-        let _batch_size = input.shape().dims::<2>()[0];
+        let batch_size = input.shape().dims::<2>()[0];
         // let negative_samples = input.shape().dims()[1] - 1;
         let log_sigmoid = sigmoid(input).log();
-        let negatives = log_sigmoid.clone().slice([0..-1, 1..-1]).sum_dim(1);
-        let positive = log_sigmoid.slice([0..-1, 0..1]);
-        return -positive.add(negatives).squeeze(1);
+        let negatives = log_sigmoid.clone().slice(s![.., 1..=-1]);
+        let negatives = negatives.sum_dim(1);
+        let positive = log_sigmoid.slice(s![.., 0]);
+        return -positive
+            .add(-negatives)
+            .div_scalar(batch_size as u32)
+            .squeeze(1);
     }
 }
 
 #[test]
-fn test_forward() {}
+fn test_forward() {
+    let dev = Default::default();
+    let model = NegativeSamplingConfig::new().init::<NdArray>(&dev);
+
+    let res = model.forward(Tensor::from_data(
+        [[1.00, 2.00, 3.00], [4.00, 5.00, 6.00]],
+        &dev,
+    ));
+
+    println!("res: {res}");
+}
