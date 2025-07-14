cec8b7b8ff1d82d226d398205d1b4a017dfbb243
fire833 <kttpsy@gmail.com>
2025-07-14 18:15:17 -0500

    feat: trying to get these plots working

--- lang-explorer-python/src/commands/nearest.py
+++ lang-explorer-python/src/commands/nearest.py
@@ -2,6 +2,8 @@
 from sklearn.neighbors import NearestNeighbors
 import pandas as pd
 from src.utils.strlen import strlen
+from graphviz import Source
+from matplotlib import pyplot as plt
 
 def nearest_neighbors(args):
 	plain = args.input.removeprefix("results/")
@@ -27,8 +29,31 @@ def nearest_neighbors(args):
 	(dist, neighind) = neigh.kneighbors(query)
 
 	programs = data.iloc[indices, 0]
+	graphs = data.iloc[indices, 1]
+
+	fig, axes = plt.subplots(len(indices), len(indices) + 1, figsize=(7, 7))
 
 	for i, prog in enumerate(programs):
 		print(f"nearest neighbors for {prog}: ")
+
+		file = f"/tmp/{i}_root.png"
+		g = Source(graphs[indices[i]], file, format="png")
+
+		axes[i][0].imshow(file)
+		axes[i][0].axis('off')
+		axes[i][0].set_title(f"Graph {i}")
+
 		for j, neigh in enumerate(dist[i]):
-			print(f"  {j + 1}: {data.iloc[neighind[i, j], 0]} {neigh}")
+			neighbor = data.iloc[neighind[i, j]]
+			ngraph = neighbor[1]
+
+			file = f"/tmp/{i}_{j}.png"
+			img = Source(ngraph, file, format="png")
+
+			axes[i][j + 1].imshow(file)
+			axes[i][0].axis('off')
+			axes[i][0].set_title(f"Graph {i}'s {j}th nearest neighbor")
+
+			print(f"  {j + 1}: {neighbor[0]} {neigh}")
+
+	plt.savefig("results/nearest_neighbors.png")
