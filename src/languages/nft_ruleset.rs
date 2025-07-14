cba61256be653ee17b16f38d3fe893bbbd452370
fire833 <kttpsy@gmail.com>
2025-07-14 17:07:48 -0500

    chore: get rid of these annoying warnings

--- src/languages/nft_ruleset.rs
+++ src/languages/nft_ruleset.rs
@@ -19,28 +19,22 @@
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-use crate::{
-    errors::LangExplorerError,
-    grammar::{elem::GrammarElement, grammar::Grammar},
-};
+use crate::{errors::LangExplorerError, grammar::grammar::Grammar};
 
-use super::{
-    strings::{nterminal_str, terminal_str, StringValue},
-    GrammarBuilder,
-};
+use super::{strings::StringValue, GrammarBuilder};
 
-nterminal_str!(NT_TABLE, "nt_table");
-nterminal_str!(NT_CHAIN, "nt_chain");
-nterminal_str!(NT_RULE, "nt_rule");
+// nterminal_str!(NT_TABLE, "nt_table");
+// nterminal_str!(NT_CHAIN, "nt_chain");
+// nterminal_str!(NT_RULE, "nt_rule");
 
-terminal_str!(TABLE, "table");
-terminal_str!(CHAIN, "chain");
-terminal_str!(ADDRESS_FAMILY_INET, "inet");
-terminal_str!(ADDRESS_FAMILY_V4, "ip");
-terminal_str!(ADDRESS_FAMILY_V6, "ip6");
-terminal_str!(ADDRESS_FAMILY_ARP, "arp");
-terminal_str!(ADDRESS_FAMILY_BRIDGE, "bridge");
-terminal_str!(ADDRESS_FAMILY_NETDEV, "netdev");
+// terminal_str!(TABLE, "table");
+// terminal_str!(CHAIN, "chain");
+// terminal_str!(ADDRESS_FAMILY_INET, "inet");
+// terminal_str!(ADDRESS_FAMILY_V4, "ip");
+// terminal_str!(ADDRESS_FAMILY_V6, "ip6");
+// terminal_str!(ADDRESS_FAMILY_ARP, "arp");
+// terminal_str!(ADDRESS_FAMILY_BRIDGE, "bridge");
+// terminal_str!(ADDRESS_FAMILY_NETDEV, "netdev");
 
 pub struct NFTRulesetLanguage;
 
