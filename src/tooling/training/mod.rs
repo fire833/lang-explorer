21ed1b99ab2544d16cc3471204f968b82e23b6e8
fire833 <kttpsy@gmail.com>
2025-08-16 16:33:52 -0500

    feat: create general training params

--- src/tooling/training/mod.rs
+++ src/tooling/training/mod.rs
@@ -15,3 +15,22 @@
 *	with this program; if not, write to the Free Software Foundation, Inc.,
 *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
+use burn::config::Config;
+
+/// Common training parameters used by most (if not all) modules.
+#[derive(Config, Debug)]
+pub struct TrainingParams {
+    /// The size of the batches fed through the model.
+    #[config(default = 256)]
+    pub batch_size: usize,
+    /// Number of epochs to train.
+    #[config(default = 10)]
+    pub n_epochs: usize,
+    /// The learning rate
+    #[config(default = 0.001)]
+    pub learning_rate: f64,
+    /// The seed.
+    #[config(default = 10)]
+    pub seed: u64,
+}
