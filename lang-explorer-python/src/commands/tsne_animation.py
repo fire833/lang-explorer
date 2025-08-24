a43192d037f313b48ba07b8d89e9128c065ed7dc
fire833 <kttpsy@gmail.com>
2025-08-23 19:26:32 -0500

    fix: animation works

--- lang-explorer-python/src/commands/tsne_animation.py
+++ lang-explorer-python/src/commands/tsne_animation.py
@@ -7,34 +7,33 @@ from matplotlib.animation import FuncAnimation
 from sklearn.manifold import TSNE
 
 def animate(args):
-	files = sorted(glob.glob(args.input))  # adjust pattern
-	snapshots = []
+    files = sorted(glob.glob(args.input))  # adjust pattern
+    snapshots = []
 
-	for f in files:
-	    print(f"Processing {f}...")
-	    data = pd.read_csv(f)  # rows = samples, cols = features (+ maybe label)
-	
-	    tsne = TSNE(n_components=2, init="random", learning_rate="auto", random_state=42)
-	    embedding = tsne.fit_transform(data.values)
-	
-	    snapshots.append(embedding)
+    init = ""
 
-	fig, ax = plt.subplots(figsize=(len(files), 1))
-	sc = ax.scatter([], [], s=10, cmap="tab10")
+    for i, f in enumerate(files):
+        print(f"Processing {f}...")
+        data = pd.read_csv(f)  # rows = samples, cols = features (+ maybe label)
 
-	def init():
-	    sc.set_offsets([])
-	    return sc,
+        tsne = TSNE(n_components=2, init="random", learning_rate="auto", random_state=42)
+        embedding = tsne.fit_transform(data.values) 
+        if i == 0:
+    	    init = embedding    
+        snapshots.append(embedding)
 
-	def update(frame):
-	    coords, labels = snapshots[frame]
-	    sc.set_offsets(coords)
-	    if labels is not None:
-	        sc.set_array(labels)
-	    ax.set_title(f"t-SNE Frame {frame+1}/{len(snapshots)}")
-	    return sc,
+    fig, ax = plt.subplots(figsize=(6, 6))
+    sc = ax.scatter(init[:,0], init[:,1], s=15, cmap="tab10")
 
-	ani = FuncAnimation(fig, update, frames=len(snapshots),
-	                    init_func=init, blit=True, interval=1000)
+    # def init():
+    #     sc.set_offsets([])
+    #     return sc,
 
-	ani.save(args.output, writer="ffmpeg", dpi=150)
+    def update(frame):
+        sc.set_offsets(snapshots[frame])
+        ax.set_title(f"t-SNE Frame {frame+1}/{len(snapshots)}")
+        return sc,
+
+    ani = FuncAnimation(fig, update, frames=len(snapshots), blit=True, interval=300)
+
+    ani.save(args.output, writer="ffmpeg", dpi=150)
