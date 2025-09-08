71dc555f4b10932992a897432a95ffb3b0cba59f
fire833 <kttpsy@gmail.com>
2025-09-08 14:02:14 -0500

    fix: cancel extra embeddings

--- lang-explorer-python/src/commands/embeddings.py
+++ lang-explorer-python/src/commands/embeddings.py
@@ -5,11 +5,12 @@ import os
 import pandas as pd
 
 def generate_embeddings(args):
-	embeddings = ["mxbai-embed-large", "nomic-embed-text", "snowflake-arctic-embed2"]
+	embeddings = []
+	# embeddings = ["mxbai-embed-large", "nomic-embed-text", "snowflake-arctic-embed2"]
 
 	print("making call to explorer")
 	res = generate("http://localhost:8080", args.language, "wmc",
-		GenerateParams(args.count, False, True, True, False, True, [], args.wl_count, args.num_neg_samples, 128, 3, 3, args.grad_clip, "Average", GeneralTrainingParameters(512, args.epochs, args.learning_rate, 0.9, args.seed, 50, False, True),
+		GenerateParams(args.count, False, True, True, False, True, embeddings, args.wl_count, args.num_neg_samples, 128, 3, 3, args.grad_clip, "Average", GeneralTrainingParameters(512, args.epochs, args.learning_rate, 0.9, args.seed, 50, False, True),
 		css=CSSLanguageParameters("exhaustivev1", ["div", "h1", "h2", "h3", "h4", "h5", "h6", "a"], ["foobar"], [
 			"#842d5b",
 	        "#20b01c",
