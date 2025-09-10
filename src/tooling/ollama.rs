62de5d8d8c8532c9f728400ae932dcf5e84aa1c7
fire833 <kttpsy@gmail.com>
2025-09-10 16:32:34 -0500

    feat: create way to make ollama requests async in bulk

--- src/tooling/ollama.rs
+++ src/tooling/ollama.rs
@@ -16,8 +16,10 @@
 *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+use futures::StreamExt;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc;
 
 use crate::{errors::LangExplorerError, languages::EmbeddingModel};
 
@@ -26,6 +28,7 @@ struct EmbeddingResult {
     embedding: Vec<f32>,
 }
 
+#[allow(unused)]
 pub(crate) async fn get_embedding_ollama(
     client: &Client,
     host: &String,
@@ -43,4 +46,56 @@ pub(crate) async fn get_embedding_ollama(
         .await?;
 
     Ok(res.embedding)
+}
+
+pub(crate) async fn get_embeddings_bulk_ollama(
+    client: &Client,
+    host: &String,
+    prompts: Vec<&String>,
+    model: EmbeddingModel,
+    num_parallel_requests: usize,
+) -> Result<Vec<Vec<f32>>, LangExplorerError> {
+    let mut results: Vec<Vec<f32>> = vec![vec![]; prompts.len()];
+
+    let req_stream = futures::stream::iter(prompts.iter().enumerate().map(
+        async |(idx, prompt)| -> Result<(usize, EmbeddingResult), LangExplorerError> {
+            let res = client
+                .post(format!("{host}/api/embeddings"))
+                .body(format!(
+                    "{{\"model\": \"{model}\", \"prompt\": \"{prompt}\"}}"
+                ))
+                .send()
+                .await?
+                .json::<EmbeddingResult>()
+                .await?;
+
+            Ok((idx, res))
+        },
+    ));
+
+    let (tx, mut rx) = mpsc::channel::<Result<(usize, EmbeddingResult), LangExplorerError>>(
+        num_parallel_requests * 2,
+    );
+
+    let _ = req_stream
+        .buffer_unordered(num_parallel_requests)
+        .for_each(|res| {
+            let txt = tx.clone();
+            async move {
+                txt.send(res).await.unwrap();
+            }
+        });
+
+    drop(tx);
+
+    while let Some(res) = rx.recv().await {
+        match res {
+            Ok(res) => {
+                results[res.0] = res.1.embedding;
+            }
+            Err(e) => return Err(e),
+        }
+    }
+
+    Ok(results)
 }
