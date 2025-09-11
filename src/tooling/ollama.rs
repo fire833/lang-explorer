017198dde34d9e568c107e5f8b02cae48c353c01
fire833 <kttpsy@gmail.com>
2025-09-11 00:48:03 -0500

    fix: ollama stuff compiles now I guess

--- src/tooling/ollama.rs
+++ src/tooling/ollama.rs
@@ -16,13 +16,12 @@
 *	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-use std::{mem, sync::Arc};
+use std::sync::Arc;
 
 use dashmap::DashMap;
 use futures::{future, StreamExt};
 use reqwest::{Client, Response};
 use serde::{Deserialize, Serialize};
-use tokio::sync::mpsc;
 
 use crate::{errors::LangExplorerError, languages::EmbeddingModel};
 
@@ -58,9 +57,9 @@ pub(crate) async fn get_embeddings_bulk_ollama(
     model: EmbeddingModel,
     num_parallel_requests: usize,
 ) -> Result<Vec<Vec<f32>>, LangExplorerError> {
-    let mut results: Arc<DashMap<usize, Vec<f32>>> = Arc::new(DashMap::new());
+    let results: Arc<DashMap<usize, Vec<f32>>> = Arc::new(DashMap::new());
 
-    let req_stream = futures::stream::iter(prompts.iter().enumerate().map(
+    let _ = futures::stream::iter(prompts.iter().enumerate().map(
         async |(idx, prompt)| -> Result<(usize, Response), LangExplorerError> {
             let res = client
                 .post(format!("{host}/api/embeddings"))
