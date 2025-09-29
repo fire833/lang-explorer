73cf073e1286df539f96f48146c58fc7b68d53cf
fire833 <kttpsy@gmail.com>
2025-09-28 22:31:57 -0500

    feat: work on code2vec impl

--- src/tooling/modules/embed/code2vec.rs
+++ src/tooling/modules/embed/code2vec.rs
@@ -48,7 +48,7 @@ impl Code2VecConfig {
         Code2Vec {
             terminals: EmbeddingConfig::new(self.n_terminals, self.d_terminal).init(device),
             paths: EmbeddingConfig::new(self.n_paths, self.d_path).init(device),
-            attention: Tensor::random([self.d_model], Distribution::Uniform(-1.0, 1.0), device),
+            attention: Tensor::random([1, self.d_model], Distribution::Uniform(-1.0, 1.0), device),
             hidden_in: LinearConfig::new(2 * self.d_terminal + self.d_path, self.d_model)
                 .with_bias(false)
                 .init(device),
@@ -66,7 +66,7 @@ pub struct Code2Vec<B: Backend> {
     /// Embeddings of all paths within the corpus.
     paths: Embedding<B>,
     /// The attention weights for code2vec.
-    attention: Tensor<B, 1, Float>,
+    attention: Tensor<B, 2, Float>,
     /// The hidden input layer.
     hidden_in: Linear<B>,
     /// The hidden output layer.
@@ -76,25 +76,52 @@ pub struct Code2Vec<B: Backend> {
 impl<B: Backend> Code2Vec<B> {
     pub fn forward(
         &self,
-        start_terminals: Tensor<B, 1, Int>,
-        end_terminals: Tensor<B, 1, Int>,
-        paths: Tensor<B, 1, Int>,
+        start_terminals: Tensor<B, 2, Int>,
+        end_terminals: Tensor<B, 2, Int>,
+        paths: Tensor<B, 2, Int>,
     ) -> Tensor<B, 2, Float> {
-        let start_emb = self.terminals.forward(start_terminals.unsqueeze_dim(1));
-        let end_emb = self.terminals.forward(end_terminals.unsqueeze_dim(1));
-        let path_emb = self.paths.forward(paths.unsqueeze_dim(1));
+        let start_emb = self.terminals.forward(start_terminals);
+        let end_emb = self.terminals.forward(end_terminals);
+        let path_emb = self.paths.forward(paths);
+        println!("path_emb: {}", path_emb);
 
-        let concat = Tensor::cat(vec![start_emb, path_emb, end_emb], 1);
+        let concat = Tensor::cat(vec![start_emb, path_emb, end_emb], 2);
+        println!("concat: {}", concat);
+
+        let num_context_vectors = concat.shape().dims[1];
+
         let hidden = self.hidden_in.forward(concat);
+
+        let attn = self
+            .attention
+            .clone()
+            .repeat_dim(0, num_context_vectors)
+            .unsqueeze_dim(0);
+
+        let hidden = hidden.mul(attn);
 
         // let attn_scores = hidden.matmul(&self.attention).softmax(0);
         // let attn_applied = hidden * attn_scores.unsqueeze(1);
         // let code_vec = attn_applied.sum_dim(0);
 
         // self.hidden_out.forward(code_vec.unsqueeze(0))
-        todo!()
+        // todo!()
+        hidden.squeeze(1)
     }
 }
 
 #[test]
-fn test_forward() {}
+fn test_forward() {
+    use burn::backend::NdArray;
+
+    let dev = Default::default();
+    let model = Code2VecConfig::new(10, 50, 30, 10, 10, 10).init::<NdArray>(&dev);
+
+    let out = model.forward(
+        Tensor::from_data([[0, 1, 2]], &dev),
+        Tensor::from_data([[3, 4, 5]], &dev),
+        Tensor::from_data([[15, 16, 17]], &dev),
+    );
+
+    println!("{}", out);
+}
