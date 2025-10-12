d41b14a7291bb03c353518a80a7a09b63de8d52a
fire833 <kttpsy@gmail.com>
2025-10-12 18:22:59 -0500

    feat: add second run for no-partials check

--- scripts/big_experiment_run.sh
+++ scripts/big_experiment_run.sh
@@ -6,3 +6,8 @@ cargo build --release --bin explorer
 ./target/release/explorer generate -l tacoexpr -e wmc -c configs/similarity.json
 ./target/release/explorer generate -l karel -e wmc -c configs/similarity.json
 ./target/release/explorer generate -l css -e wmc -c configs/similarity.json
+
+./target/release/explorer generate -l tacosched -e wmc -c configs/similarity_nopartials.json
+./target/release/explorer generate -l tacoexpr -e wmc -c configs/similarity_nopartials.json
+./target/release/explorer generate -l karel -e wmc -c configs/similarity_nopartials.json
+./target/release/explorer generate -l css -e wmc -c configs/similarity_nopartials.json
